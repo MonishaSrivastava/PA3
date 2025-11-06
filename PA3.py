2b5b3b3ce06a171e380645ba74b880b011910b5a
MonishaSrivastava <msrivastava@madeira.org>
2025-11-06 08:36:28 -0500

    added some inline comments on the most important things--will go back and add links to things that i have got off of google.

--- PA3.py
+++ PA3.py
@@ -2,52 +2,52 @@
 #BUDGET PLANNER
 #-------------------
 
-import datetime
+import datetime #allows python to import the current date/time https://www.w3schools.com/python/python_datetime.asp
 
-expenses_file = "expenses.txt"
+expenses_file = "expenses.txt" #name of the file where everything will be saved
 
 def add_expenses(expenses):
-    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") #datetime.now returns the name of the weekday  and datetime.strftime is used for formatting, it takes 1 parameter to specify the format of the returned string. (ex. %Y = year)
     print("\nAvailable categories:")
-    for category in expenses:
+    for category in expenses: #goes through every key (category) in expenses
         print(f"{category}") #Prints out all categories
 
-    category_name = ""
+    category_name = "" #stores the chosen category name later 
 
     while True:
         choice = input("Which category do you want to add expenses to? (Type the number) > ")
 
-        for category in expenses:
-            if category.startswith(choice + "."):
+        for category in expenses: #goes through each category to find one matching the number entered.
+            if category.startswith(choice + "."): #if statement: verifies input matches a valid category number. Startwith() function method return true if string (category) starts with the specified value (1)--python uses this to verify if the category starts with 1, 2, 3, etc
                 category_name = category
-                break
+                break #exits the loop once verified true
 
-        if category_name != "":
+        if category_name != "": #true if a valid category was found
             print(f"\nYou selected: {category_name}")
-            break #exit once valid
+            break #exit while loop once valid
         else:
             print("\nInvalid category number!")
             break
     
-    if category_name == "":
+    if category_name == "": #stops function if nothing typed or if no valid category was chosen
         print("No valid category selected.")
         return
 
-    try:
-        amount = float(input("Enter the amount you spent: $"))
-    except ValueError:
+    try: #attempts to convert user input into a float
+        amount = float(input("Enter the amount you spent: $")) #float is used to return decimal points ex. if you type 138.89 it won't show up as error as it would if you removed the float https://www.w3schools.com/python/ref_func_float.asp
+    except ValueError: #runs if user enters a non-numeric number
         print("\nInvalid input. Please enter a number.")
         return
 
     note = input("Add a short note (recommended) > ")
 
-    expenses[category_name].append((amount, note, date))
+    expenses[category_name].append((amount, note, date)) #adds tuple to selected category (yay i learnt tuples last proj)
     print(f"\nAdded ${amount:.2f} to {category_name}!")
 
 def save_expenses(filename, expenses):
     with open(filename, "w") as f: #opens file in write mode and overwrites the file with users budget/expenses
-        for category, items in expenses.items():
-            for amount, note, date in items:
+        for category, items in expenses.items(): #goes through each category and its list of expenses
+            for amount, note, date in items: #nested for loop (for loop inside for loop) this writes each expense tuple
                 f.write(f"{category},{amount},{note},{date}\n") #overwrites each category, amount, and note in the expenses.txt file
     print("All expenses are saved!\n")
 
@@ -56,28 +56,28 @@ def view_summary(expenses, limits):
     print("-" * 60)
 
     total_expenses = 0
-    user_has_expenses = False
+    user_has_expenses = False #if user doesn't have spending it will be false
 
-    for category, items in expenses.items():
+    for category, items in expenses.items(): #checks each spending category
         if items:  # show only if user actually spent here
             user_has_expenses = True
-            total = sum(amount for amount, note, date in items)
+            total = sum(amount for amount, note, date in items) #generator inside sum() totals all expenses
             total_expenses += total
-            limit = limits.get(category, 0)
+            limit = limits.get(category, 0) #retrieves category limit (default = 0). .get() returns the value (0) of the item with a specified key(category) https://www.w3schools.com/python/ref_dictionary_get.asp -- without this it would show an error for all limits set to 0 such as category health
 
-            for amount, note, date in items:
+            for amount, note, date in items: #displays each individual expense record
                 print(f"${amount:.2f} on {date} ({note}) spent (Limit: ${limit:.2f})")  
 
             if total > limit: #if the total is higher than the limit, it will make a little caution note saying "Over Budget!"
                 print("Over budget!")
 
-            elif total < limit:
+            elif total < limit: #shows how much money remains under the limit
                 print(f"${limit - total:.2f} remaining.")
 
-    if not user_has_expenses:
-        print("No expenses recorded yet.")
+    if not user_has_expenses: #without this it won't run if the user didn't have expenses 
+        print("No expenses recorded yet.") #will show this if user does not have any expenses yet
 
-    else:
+    else: #runs when user has expenses
             print("-" * 60)
             print(f"Total spent across all categories: ${total_expenses:.2f}")
 
@@ -85,58 +85,58 @@ def view_summary(expenses, limits):
 
 def add_limits(limits):
     print("\nLimits set:")
-    for category, limit in limits.items(): 
+    for category, limit in limits.items(): #prints all category-limit pairs
         print(f"{category} : ${limit}")
 
-    while True:
+    while True: #continues until a valid category is selected
         choice = input("\nEnter the category number to change > ")
 
         category_name = ""
-        for category in limits:
-            if category.startswith(choice + "."):
+        for category in limits: #searches for a category starting with the entered number
+            if category.startswith(choice + "."): #verifies if input matches an existing category
                 category_name = category
                 break
 
-        if category_name != "":
+        if category_name != "": #if its a valid input path it will display your category name and move on to the next question
             print(f"\nYou selected: {category_name}")
             break
         else:
             print("Invalid category number!")
 
-    try:
+    try: #attempts to convert user number into float
         new_limit = float(input(f"Enter new limit for {category_name}: $")) #float basically adds decimals points ex. instead of 10 it would return as 10.0
 
-    except ValueError:
+    except ValueError: #handles invalid number input
         print("\nInvalid input. Please enter a number.")
         return
 
-    limits[category_name] = new_limit
+    limits[category_name] = new_limit #updates chosen limit
     print(f"\nUpdated limit for {category_name} to ${new_limit:.2f}")
 
 def load_expenses(filename, expenses):
-    try:
+    try: #attempt to open and read the saved file
         with open(filename, "r") as f: #opens file in read mode
-            for line in f:
+            for line in f: #reads the file line for line
                 # Remove any extra spaces or newlines from the line
                 clean_line = line.strip()
 
                 # Split the line by commas into separate parts
                 parts = clean_line.split(",")
 
-                # Each line should have 4 parts: category, amount, note, and date
-                if len(parts) != 4:
+                # Each line should have 4 parts: category, amount, note, and date. len function returns the number of characters in the string
+                if len(parts) != 4: #skips incomplete lines https://www.w3schools.com/python/ref_func_len.asp
                     continue  # move to next line if data is missing or broken
 
                 category = parts[0]
                 amount_text = parts[1]
                 note = parts[2]
                 date = parts[3]
-                try:
+                try: #attempts to convert amount into a float -- 10 to 10.00
                     amount = float(amount_text)
                 except ValueError:
-                    continue  # skip bad data
-                if category in expenses:
-                    expenses[category].append((amount, note, date))
+                    continue  # skip bad data/entries with invalid input
+                if category in expenses: #adds the expense if category is valid
+                    expenses[category].append((amount, note, date)) #tuple
         print("\nPrevious expenses loaded successfully!\n")
     except FileNotFoundError:
         print("\nNo previous expense file found. Starting fresh!\n")
@@ -145,6 +145,8 @@ def load_expenses(filename, expenses):
 def main():
     name = input("Hello! Please enter your name > ")
     print(f"\nHello {name}!")
+
+#dictionary for categories and expense bracket
 
     expenses = {
             "1. electronics": [],
@@ -162,7 +164,7 @@ def main():
             "13. pet necessities (ex. cat food)": [],
             "14. taxes/loans": []
         }
-        #limits
+        #limit dictionary
     limits = {
             "1. electronics": 50,
             "2. clothing/shoes": 20,
@@ -180,8 +182,10 @@ def main():
             "14. taxes/loans": 0
         }
 
-    load_expenses(expenses_file, expenses)
-    while True:
+    load_expenses(expenses_file, expenses) # load previous data before showing menu
+
+
+    while True: #keeps running until user types 4 (quit)
         print("\nWhat would you like to do > ")
         print("1. Add an expense")
         print("2. View Summary")
@@ -190,22 +194,22 @@ def main():
 
         choice = input("Choose a number > ")
 
-        if choice == "1":
+        if choice == "1": #user selects to add expense
             add_expenses(expenses)
 
-        elif choice == "2":
+        elif choice == "2": #user selects to view the summary of their expenses and if they are over the budget
             view_summary(expenses, limits)
 
-        elif choice == "3":
+        elif choice == "3": #user selects to add limits
             add_limits(limits)
 
-        elif choice == "4":
+        elif choice == "4": #user selects to save expenses and quits
             save_expenses(expenses_file, expenses)
             print("Your expenses have been saved. Goodbye!")
             break
-        else:
+        else: #invalid user input
             print("Sorry! Either type: 1, 2, 3, 4\n")
-            continue
+            continue #restarts the while loop and prints out the options initally given
 
 
 #To run the code
